062d33f41b70137b5e166442b9adc840c803948a
borhez <getlink32@gmail.com>
2024-07-13 14:19:28 +0300

    Fill inventory hosts.yaml

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Instruction  
 
 ### Prepare:
-- Run manually start.sh from directory "prepare". With arg=N amount docker-container's hosts. Will be create users "ansible". Remember password during this, because it will be need type password during command "ssh-copy-id".
-- Start Jenkins job.
+- Run manually start.sh from directory "prepare". With arg=N amount docker-container's hosts. Will be create users "ansible". Remember password during this, because it will be need type password during command "ssh-copy-id".  
+- it will be filled inventory  
+- Start Jenkins job.  
 
--- prepare/src/server_local.sh
+++ prepare/src/server_local.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
-AMOUNT_HOSTS=$1
 CONTAINERS_NET=$2
+let "nSegments=$1 - 2"
 
+echo $nSegments
+echo $CONTAINERS_NET
 echo -e "\033[35mupdate ansible-server on localhost (${HOSTNAME})\033[30m"
 
 netID=$(docker network ls |grep $CONTAINERS_NET | awk '{print $1}')
-patternIP=$(ip addr show | grep "inet.*$CONTAINERS_NET.*" | awk '{print $2}' | cut -d"/" -f1 | sed '$ s/.$//')
+patternIP=$(ip addr show | grep "inet.*$netID.*" | awk '{print $2}' | cut -d"/" -f1 | sed '$ s/.$//')
+
+echo -e -n "segments:\n" > ../ansible/hosts.yaml
 n=1
 endIP=2   #ip for container starts from 2 (172.20.0.2)
-while [[ $n -le ${AMOUNT_HOSTS} ]]
+while [[ $n -le ${nSegments} ]]
 do
   echo "put ssh-key on ${patternIP}$endIP"
   ssh-copy-id ansible@${patternIP}$endIP
+  echo -e "\thost$n:\n\t\tip: ${patternIP}$endIP\n" >> ../ansible/hosts.yaml
   (( n++ ))
   (( endIP++ ))
-done 
+done
+
+  echo "put ssh-key on Master ${patternIP}$endIP"
+  ssh-copy-id ansible@${patternIP}$endIP
+  echo -e "\nmaster:\n\thost$n:\n\t\tip: ${patternIP}$endIP\n" >> ../ansible/hosts.yaml
+  
+  (( n++ ))
+  (( endIP++ ))
+
+  echo "put ssh-key on Standby ${patternIP}$endIP"
+  ssh-copy-id ansible@${patternIP}$endIP
+  echo -e -n "\nstandbymaster:\n\thost$n:\n\t\tip: ${patternIP}$endIP\n" >> ../ansible/hosts.yaml
+
